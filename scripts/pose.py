8c78ffffdca3aa59a6898d098f6835cf67aac470
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-08 16:05:29 +0900

    [UPD] rename main function

--- scripts/pose.py
+++ scripts/pose.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
+
 import rospy
 from sobit_mini_module import SobitMiniJointController
 from sobit_mini_module import Joint
 import sys
 import math
 
-def test():
+def main():
     rospy.init_node('sobit_mini_initial_pose')
     args = sys.argv
     mini_ctr = SobitMiniJointController(args[0]) # args[0] : Arguments for ros::init() on C++
 
-    # Strike a certain pose.
+    # Set initial pose
     mini_ctr.moveToPose( "initial_pose" )
 
 if __name__ == '__main__':
     try:
-        test()
+        main()
     except rospy.ROSInterruptException: pass
